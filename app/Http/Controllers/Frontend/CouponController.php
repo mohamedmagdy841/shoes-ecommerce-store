bd9491ffe01ef779e7c42e11691a6409df360770
magdy <mmagdy841@gmail.com>
2025-02-06 07:49:34 +0200

    refactor coupon controller

--- app/Http/Controllers/Frontend/CouponController.php
+++ app/Http/Controllers/Frontend/CouponController.php
@@ -9,7 +9,7 @@ use App\Services\CouponValidator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Number;
-
+use Cart;
 class CouponController extends Controller
 {
     protected $couponValidator;
