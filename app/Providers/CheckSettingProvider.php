a0dd16b16a6baf8bb0a5872a222f3318cd902a9e
Mohamed Magdy Gamal <64127744+mohamedmagdy841@users.noreply.github.com>
2025-04-19 10:46:09 +0200

    making testing env

--- app/Providers/CheckSettingProvider.php
+++ app/Providers/CheckSettingProvider.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Setting;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class CheckSettingProvider extends ServiceProvider
 {
@@ -22,29 +23,31 @@ class CheckSettingProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $getSetting = Setting::firstOr(function (){
-            return Setting::create([
-                'site_name' => 'Karma',
-                'email' => 'karma@gmail.com',
-                'phone' => '01234567891',
-                'about_us' => 'Karma is an online store selling shoes for men, women and kids',
-                'favicon' => 'default',
-                'logo' => '/img/logo.png',
-                'facebook' => 'https://www.facebook.com',
-                'x' => 'https://x.com',
-                'instagram' => 'https://www.instagram.com',
-                'youtube' => 'https://www.youtube.com',
-                'street' => '17th street',
-                'city' => 'San Francisco',
-                'country' => 'USA',
+        if (Schema::hasTable('settings')) {
+            $getSetting = Setting::firstOr(function (){
+                return Setting::create([
+                    'site_name' => 'Karma',
+                    'email' => 'karma@gmail.com',
+                    'phone' => '01234567891',
+                    'about_us' => 'Karma is an online store selling shoes for men, women and kids',
+                    'favicon' => 'default',
+                    'logo' => '/img/logo.png',
+                    'facebook' => 'https://www.facebook.com',
+                    'x' => 'https://x.com',
+                    'instagram' => 'https://www.instagram.com',
+                    'youtube' => 'https://www.youtube.com',
+                    'street' => '17th street',
+                    'city' => 'San Francisco',
+                    'country' => 'USA',
+                ]);
+            });
+
+            $categories = Category::select('slug', 'name')->get();
+
+            View::share([
+                'getSetting' => $getSetting,
+                'categories' => $categories,
             ]);
-        });
-
-        $categories = Category::select('slug', 'name')->get();
-
-        View::share([
-            'getSetting' => $getSetting,
-            'categories' => $categories,
-        ]);
+        }
     }
 }
